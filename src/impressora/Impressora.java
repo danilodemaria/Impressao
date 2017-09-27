af6912fe82b4da59f0a67ba9eb734285add907d0
Danilo de Maria <daniloctg@msn.com>
2017-09-27 09:50:16 -0300

    Commit Inicial

---
+++ src/impressora/Impressora.java
@@ -0,0 +1,28 @@
+/*
+ * To change this license header, choose License Headers in Project Properties.
+ * To change this template file, choose Tools | Templates
+ * and open the template in the editor.
+ */
+package impressora;
+
+
+/**
+ *
+ * @author Administração
+ */
+public class Impressora {
+
+    /**
+     * @param args the command line arguments
+     */
+    
+
+    public static void main(String[] args) {
+        Menu a = new Menu();
+        a.setVisible(true);
+        
+    }
+
+   
+
+}
